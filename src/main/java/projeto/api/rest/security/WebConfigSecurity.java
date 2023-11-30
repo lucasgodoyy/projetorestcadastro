d38acf113dff9f780e226784979739ecd4096720
lucasgodoyy <ehlucasgodoy10@gmail.com>
2023-11-30 18:17:04 -0300

    classe JWTTokenAutenticacaoService e ApplicationContext

---
+++ src/main/java/projeto/api/rest/security/WebConfigSecurity.java
@@ -0,0 +1,16 @@
+package projeto.api.rest.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebConfigSecurity extends WebSecurityConfigurerAdapter {
+	
+	
+	
+	
+	
+}
+	
